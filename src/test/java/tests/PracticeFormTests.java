0b6e7dcb92a007736f73e2f539647c8266c9ca7d
nasty <nastyushas18.93@gmail.com>
2026-08-11 20:12:50 +0300

    Start

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,22 +1,31 @@
 package tests;
 
 import dto.Student;
+import enums.Gender;
+import enums.Hobbies;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.FormsPage;
 import pages.HomePage;
 import pages.PracticeFormPage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PracticeFormTests extends AppManager {
     @Test
     public void practiceFormPositiveTest() {
+        List<Hobbies> hobbies = new ArrayList<>();
+        hobbies.add(Hobbies.SPORTS);
+        hobbies.add(Hobbies.MUSIC);
         Student student = Student.builder()
                 .firstName("John")
                 .lastName("Doe")
                 .email("john678@gmail.com")
-                .gender("male")
+                .gender(Gender.MALE)
                 .mobile("0123456789")
-                .dateOfBirth("22 April 1987").subjects("Math,Chemistry,English")
+                .dateOfBirth("22 April 1987")
+                .subjects("Math,Chemistry,English")
                 .hobbies("sport")
                 .picture("")
                 .address("Street 1")
